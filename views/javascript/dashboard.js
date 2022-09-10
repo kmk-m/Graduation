3610e2fba1e8ac1c8b12c199ac44ab354ea56d44
mogamal1438 <mogamal1438@gmail.com>
2022-09-10 21:40:33 +0200

    myedit

--- views/javascript/dashboard.js
+++ views/javascript/dashboard.js
@@ -57,7 +57,7 @@ function data(json) {
     var video = document.createElement("div");
     video.classList.add("video");
     video.innerHTML =
-      "<p> Welcome to our Site </p>" + "<video width=95% height=100% controls>  <source src=" +
+      "<p> Paragraph </p>" + "<video width=95% height=100% controls>  <source src=" +
       json.data.videos[i] +
       "#t=0.6" +
       " type=video/mp4></video>";
