c12ae763da3b8618ea914d55cbb0555e9c443bed
jokerf6 <fhakem75@gmail.com>
2022-09-26 01:31:29 +0200

    edit databases

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import Cors from "cors";
 import passport from "passport";
 import FacebookStrategy from "passport-facebook";
 import GoogleStrategy from "passport-google-oauth2";
-import jwt from "./util/jwt";
+import jwt from "../util/jwt.js";
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
