52a3a57b5ef52919b4c8b1f2f8beca9a3eaa80ba
Amanymohamedsayed <amanymohamed20011@gmail.com>
2022-09-30 19:00:16 +0200

    some update

--- views/javascript/dashboard.js
+++ views/javascript/dashboard.js
@@ -14,71 +14,93 @@ fetch("http://127.0.0.1:3000/dashboard", {
     "Content-type": "application/json; charset=UTF-8",
   },
 })
-  .then((response) => response.json())
-  .then((json) => data(json));
+
+.then((response) => response.json())
+.then((json) => data(json));
 function data(json) {
-  let tracks = json.data.tracks;
-  tracks.forEach((x) => {
-    let link = document.createElement("a");
-    link.innerText = x.name;
-    link.href = "http://127.0.0.1:3000/Tracks/" + x.trackId;
-    let parent = document.getElementById("myDropdown");
-    parent.appendChild(link);
-  });
-  tracks.forEach((x) => {
-    let link = document.createElement("a");
-    link.innerText = x.name;
-    link.href = "http://127.0.0.1:3000/Tracks/" + x.trackId;
-    let parent1 = document.getElementById("myDropdown1");
-    parent1.appendChild(link);
-  });
-  var img = document.createElement("img");
+  /* return name */
+  console.log(json.data.User.firstName + " " + json.data.User.lastName);
+  let name = json.data.User.firstName + " " + json.data.User.lastName;
+  const para = document.createElement("div");
+  para.innerHTML = "<h5>" + name + "</h5>";
+  let element = document.getElementById("practice");
+  element.appendChild(para);
+  para.classList.add("op");
+  /* return image */
+  let img = document.createElement("img");
   img.src = json.data.User.image;
-  var element = document.getElementById("icons");
-  element.appendChild(img);
-  element.insertBefore(img, element.firstChild);
-  var hacks = json.data.hackathons;
-  for (let i = 0; i < hacks.length; i += 1) {
-    var box = document.createElement("div");
-    var time = new Date();
-    var str = time.toLocaleString({
-      day: "numeric",
-      month: "numeric",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-    const currdate = new Date(str);
-    const date = new Date(hacks[i].date);
-    var diff = date - currdate;
-    const diffDays = Math.ceil(diff / (1000 * 60 * 60 * 24) - 1);
-    var min = Math.ceil(diff / (1000 * 60) - diffDays * 60 - 120);
-    box.classList.add("box");
-    box.innerHTML =
-      "<h3>Before Hackathon</h3><a href=#>" +
-      hacks[i].name +
-      " Round#" +
-      hacks[i].round +
-      "</a> <h4>" +
-      "Days " +
-      diffDays +
-      "</h4>";
-    var element = document.getElementById("boxes");
-    element.appendChild(box);
-  }
-  for (let i = 0; i < json.data.videos.length; i += 1) {
-    console.log(json.data.videos[i]);
-    var video = document.createElement("div");
-    video.classList.add("video");
-    video.innerHTML =
-      "<p>" +
-      json.data.videos[i].comment +
-      "  </p>" +
-      "<video width=95% height=100% controls>  <source src=" +
-      json.data.videos[i] +
-      "#t=0.6" +
-      " type=video/mp4></video>";
-    var element = document.getElementById("all");
-    element.appendChild(video);
-  }
+  let elementimg = document.getElementById("practice");
+  elementimg.appendChild(img);
+  img.classList.add("image1");
+  img.alt = "profile";
+  img.title = "profile";
+  img.setAttribute("id", "image1");
+  console.log(img);
 }
+
+/* name of hack 
+function data(json) {
+  console.log(json.data.hackathons.name );
+  let namehac = json.data.hackathons.name ;
+  const parahac = document.createElement("div");
+  para.innerHTML = "<h5>" + namehac + "</h5>";
+  let elementhac = document.getElementById("hac");
+  elementhac.appendChild(parahac);
+  parahac.classList.add("namehack");
+  
+}
+*/
+/* Hakthon countdown */
+let countDownDate = new Date("nov 26, 2022 23:59:59").getTime();
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff = countDownDate - dateNow;
+  // Get Time Units
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+  let str=" Days";
+  let str2= " : ";
+  let str3=" : ";
+  if ( days>0 ){
+      document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+      document.querySelector(".day").innerHTML = str ;
+  }
+  else {
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours} :` : `${hours} :` ;
+  document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes} :` :`${minutes} :` ;
+  document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+  }
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
+/* countdown */
+let countDownDate2 = new Date("date").getTime();
+let counter2 = setInterval(() => {
+  // Get Date Now
+  let dateNow2 = new Date().getTime();
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff2 = countDownDate2 - dateNow2;
+  // Get Time Units
+  let days2 = Math.floor(dateDiff2 / (1000 * 60 * 60 * 24));
+  let hours2 = Math.floor((dateDiff2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes2 = Math.floor((dateDiff2 % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds2 = Math.floor((dateDiff2 % (1000 * 60)) / 1000);
+  let str2="  Days";
+  if ( days2>0 ){
+      document.querySelector(".days2").innerHTML = days2 < 10 ? `0${days2}` : days2;
+      document.querySelector(".day2").innerHTML = str2 ;
+  }
+  else {
+  document.querySelector(".hours2").innerHTML = hours2 < 10 ? `0${hours2} :` : `${hours2} :` ;
+  document.querySelector(".minutes2").innerHTML = minutes2 < 10 ? `0${minutes2} :` :`${minutes2} :` ;
+  document.querySelector(".seconds2").innerHTML = seconds2 < 10 ? `0${seconds2}` : seconds2;
+  }
+  if (dateDiff2 < 0) {
+    clearInterval(counter2);
+  }
+}, 1000);
