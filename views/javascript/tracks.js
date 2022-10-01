76aa25bd401a8a81a74a3e410ea2867f6fa00c60
jokerf6 <fhakem75@gmail.com>
2022-10-01 07:30:56 +0200

    finish homw course

--- views/javascript/tracks.js
+++ views/javascript/tracks.js
@@ -1,7 +1,7 @@
 let id = window.location.href;
 id = id.slice(id.lastIndexOf("/") + 1);
 let ass = document.getElementById("ass");
-ass.href = `http://127.0.0.1:3000/Tracks/${id}/Assignments`;
+//ass.href = `http://127.0.0.1:3000/Tracks/${id}/Assignments`;
 console.log(id);
 fetch("http://127.0.0.1:3000/Tracks/data/" + id, {
   method: "GET",
@@ -13,6 +13,12 @@ fetch("http://127.0.0.1:3000/Tracks/data/" + id, {
   .then((json) => data(json));
 function data(json) {
   /* return name */
+  user(json);
+  setTrackData(json.data.track);
+  setCourses(json.data.TrackCourses);
+  //console.log(json.data);
+}
+function user(json) {
   console.log(json.data.User.firstName + " " + json.data.User.lastName);
   let name = json.data.User.firstName + " " + json.data.User.lastName;
   const para = document.createElement("div");
@@ -51,3 +57,107 @@ function data(json) {
     window.location.href = "https:127.0.0.1:3000/profile";
   }
 }
+function setTrackData(track) {
+  // console.log(track.image);
+  document.getElementById("header").style.backgroundImage =
+    "url(" + track.image + ")";
+  document.getElementById("track_name").innerHTML = track.name;
+  // introVideo
+  document.getElementById("header").addEventListener("click", () => {
+    window.location.href = "http:#";
+  });
+}
+function setCourses(courses) {
+  let gridContainer = document.getElementById("gridContainer");
+  for (let index of courses) {
+    let course = index;
+    //GridElement
+    let gridItemElement = document.createElement("div");
+    let grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.classList.add("lay");
+    gridItemElement.appendChild(grid);
+    gridItemElement.classList.add("grid-item");
+    gridItemElement.id = index;
+
+    if (course.show == 0) {
+      console.log(course.show);
+    } else {
+      console.log(course.show);
+
+      gridItemElement.classList.add("opp");
+    }
+
+    grid.style.backgroundImage = `url(${course.Course.image})`;
+
+    //overlay
+
+    // adding gridElement to gridContainer
+    gridContainer.appendChild(gridItemElement);
+    var overlayDiv = createOverlayDiv(course);
+    gridItemElement.appendChild(overlayDiv);
+
+    // adding childs to Parent
+  }
+}
+
+function createOverlayDiv(course) {
+  let par = document.querySelector(".grid-item");
+  // childs
+  let det = document.createElement("div");
+  det.classList.add("det");
+  console.log(par);
+  let courseNameElement = document.createElement("p");
+  courseNameElement.innerHTML = course.Course.name;
+
+  let courseInstructorElement = document.createElement("p");
+  courseInstructorElement.innerHTML = course.Course.instructor;
+  courseInstructorElement.classList.add("ins");
+  let courseDescriptionElement = document.createElement("div");
+  let x = ((course.Course.rating * 5) / 100 / 5) * 100;
+  console.log(x * 2.7);
+  courseDescriptionElement.innerHTML = `
+  <div class = "flex"> 
+  <sanp class = "sta">${
+    ((course.Course.rating * 5) / 100).toString().length === 1
+      ? ((course.Course.rating * 5) / 100).toString()[0] + ".0"
+      : ((course.Course.rating * 5) / 100).toString()[0] +
+        "." +
+        ((course.Course.rating * 5) / 100).toString()[2]
+  } </sanp>
+  <div class="star-ratings">
+  
+  <div class="fill-ratings" style="width: ${x}%;">
+    <span>★★★★★</span>
+  </div>
+  <div class="empty-ratings">
+    <span>★★★★★</span>
+  </div>
+</div> 
+</div>
+`;
+
+  // append to parent
+  det.appendChild(courseNameElement);
+  det.appendChild(courseInstructorElement);
+  det.appendChild(courseDescriptionElement);
+  par.appendChild(det);
+  return det;
+}
+function playTrackVideo() {
+  window.alert("play track video");
+
+  // document.getElementById('trackVideoModal').style.display='block';
+
+  // var video = document.getElementById('videoPlayer');
+  // video.src = trackData.introVideo;
+  // video.play();
+}
+$(document).ready(function () {
+  // Gets the span width of the filled-ratings span
+  // this will be the same for each rating
+  var star_rating_width = $(".fill-ratings span").width();
+  // Sets the container of the ratings to span width
+  // thus the percentages in mobile will never be wrong
+  $(".star-ratings").width(star_rating_width);
+});
