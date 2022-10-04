2a9c329531b8bf192ba192b4602893601ad0ec60
Amanymohamedsayed <amanymohamed20011@gmail.com>
2022-10-04 18:23:09 +0200

    amoji

--- util/connection.js
+++ util/connection.js
@@ -9,17 +9,18 @@
    password: "3e689dd2",
    logging: false,
  });
- export { connection as default };
-*/
+ export { connection as default };*/
+
 
 import { Sequelize } from "sequelize";
 
 const connection = new Sequelize({
   dialect: "mysql",
-  host: "127.0.1",
+  host: "127.0.0.1",
   database: "lms",
   username: "root",
   password: "",
   logging: false,
 });
 export { connection as default };
+
