d0e2e75c9481f3b6b0b53ef7b36f7d8929acef7a
mogamal1438 <mogamal1438@gmail.com>
2022-10-05 09:35:21 +0200

    edit

--- util/connection.js
+++ util/connection.js
@@ -2,10 +2,10 @@ import { Sequelize } from "sequelize";
 
 const connection = new Sequelize({
   dialect: "mysql",
-  host: "127.0.1",
-  database: "lms",
-  username: "root",
-  password: "",
+  host: "us-cdbr-east-06.cleardb.net",
+  database: "heroku_e41c1baee36a515",
+  username: "b9e60d1985c298",
+  password: "3e689dd2",
   logging: false,
 });
 export { connection as default };
