54f17529ba53955b9f709439292948d8af736037
jokerf6 <fhakem75@gmail.com>
2022-10-05 07:40:44 +0200

    fix form

--- util/connection.js
+++ util/connection.js
@@ -1,14 +1,14 @@
-//  import { Sequelize } from "sequelize";
+// import { Sequelize } from "sequelize";
 
-//  const connection = new Sequelize({
-//    dialect: "mysql",
-//    host: "us-cdbr-east-06.cleardb.net",
-//    database: "heroku_e41c1baee36a515",
-//    username: "b9e60d1985c298",
-//    password: "3e689dd2",
-//    logging: false,
-//  });
-//  export { connection as default };*/
+// const connection = new Sequelize({
+//   dialect: "mysql",
+//   host: "us-cdbr-east-06.cleardb.net",
+//   database: "heroku_e41c1baee36a515",
+//   username: "b9e60d1985c298",
+//   password: "3e689dd2",
+//   logging: false,
+// });
+// export { connection as default };
 
 import { Sequelize } from "sequelize";
 
