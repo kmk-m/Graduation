641627601a6838526af6dbcb1b2d41d440374eaf
jokerf6 <fhakem75@gmail.com>
2022-10-11 20:37:00 +0200

    add course Final project

--- controllers/courses/getIntroVideo.js
+++ controllers/courses/getIntroVideo.js
@@ -1,3 +1,28 @@
 import Responses from "../../util/response.js";
 
-async function getIntroVideo(req, res, next) {}
+async function getIntroVideo(req, res, next) {
+  const { Projects, userProjects } = req.models;
+  const { courseId } = req.params;
+
+  const Requirements = await Projects.findOne({
+    where: {
+      courseId,
+    },
+    attributes: ["projectId", "requirement", "name"],
+  });
+  if (!Requirements) {
+    return Responses.badRequest(res, "course not found");
+  }
+  const lastSubmission = await userProjects.findOne({
+    where: {
+      userId: req.userId,
+      projectId: Requirements.projectId,
+    },
+    attributes: ["status", "Comment"],
+  });
+  return Responses.success(res, "Get data Successfully", {
+    Requirements,
+    lastSubmission,
+  });
+}
+export default getIntroVideo;
